078c7b3d70953a9c9636f9795d85154714eb2428
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-18 08:51:26 -0500

    add console.log

--- addToZero.js
+++ addToZero.js
@@ -6,3 +6,8 @@ for (let i = 0; i < array.length; i++){
     for (let a = 0; a < array.length; a++) {
         if (i !== a) {
             if (array[i] + array[a] === 0)
+            value = "True"
+      } 
+    }
+  }
+console.log(value)
