a9e5dd469056ac05b6808fdbc570595cb8940993
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-18 08:58:05 -0500

    cleaned up {}

--- addToZero.js
+++ addToZero.js
@@ -2,12 +2,12 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 let value = "False"
+
 for (let i = 0; i < array.length; i++){
-    for (let a = 0; a < array.length; a++) {
-        if (i !== a) {
-            if (array[i] + array[a] === 0)
+}for (let a = 0; a < array.length; a++) {
+    } if (i !== a) {
+    }if (array[i] + array[a] === 0)
             value = "True"
-      } 
-    }
-  }
+ 
+  
 console.log(value)
