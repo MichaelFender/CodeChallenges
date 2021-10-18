2428d9a1545eed40f797a304a062582cb9b6d379
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-18 08:49:08 -0500

    add second  loop

--- addToZero.js
+++ addToZero.js
@@ -2,4 +2,5 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 let value = "False"
-for (let i = 0; i < array.length; i++)
+for (let i = 0; i < array.length; i++){
+    for (let a = 0; a < array.length; a++) {
