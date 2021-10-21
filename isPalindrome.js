0af6f690af1e6141b70663944ccd42c42fead796
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-21 09:41:33 -0500

    PALINDROME DONE

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,14 @@
 // Write your code below
+function isPalindrome(word) {
+    let j = word.length - 1
+    for (let i = 0; i < (word.length - 1) / 2; i++) 
+      if (word[i] !== word[j]) 
+        return false
+        return true
+  }
+  let input = "booB"
+  
+  if (isPalindrome(input) == true)
+      console.log(input +  " is palindrome")
+  else
+      console.log(input + " is not a palindrome")
