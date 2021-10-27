69e25b372654d42037382d3fe9fa385af9a34625
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-27 08:39:00 -0500

    line 6 if statement

--- stringDecoder.js
+++ stringDecoder.js
@@ -2,5 +2,9 @@ const decoder = code => {
     let stringSplit = code.split("")
     let result = ""
 
-    for (let i=0; i<stringSplit.length; i++)
+    for (let i=0; i<stringSplit.length; i++) {
+        if (!isNaN(+stringSplit[i])){
+            i += +stringSplit[i]
+        }
+    }
 }
