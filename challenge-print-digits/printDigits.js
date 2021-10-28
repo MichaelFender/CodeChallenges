1c206b0af2fe4891fb04090fa3ec987f937df999
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-28 09:30:47 -0500

    Write a function called printDigits that takes in one argument, num. When given an integer, the function should print (console.log) each digit in reverse order, starting with the ones place

--- challenge-print-digits/printDigits.js
+++ challenge-print-digits/printDigits.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const printDigits = num => {
+    while (num) {
+        let digit = num % 10
+        console.log(digit)
+        num = (num - digit) / 10
+    }
+    
+}
+
+printDigits(1)
+printDigits(314)
+printDigits(12)
--- stringDecoder.js
+++ stringDecoder.js
@@ -1,14 +1,19 @@
 const decoder = code => {
     let stringSplit = code.split("")
     let result = ""
-
+    
     for (let i=0; i<stringSplit.length; i++) {
         if (!isNaN(+stringSplit[i])){ i += +stringSplit[i] 
         } else{
-
+            result += stringSplit[i]
         }
+       
     }
+    console.log(result);
     return result
+    
 }
 
-
+decoder('0A4akjfSS')
+decoder('0O2xcM5ngbrdG')
+decoder('2bnL0U1mC2osK0Y')
