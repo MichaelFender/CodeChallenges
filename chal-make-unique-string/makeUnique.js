2ddad822ebfe0a1b89ad6639cffd1c8fb610f16a
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-02 09:05:02 -0500

    got it

--- chal-make-unique-string/makeUnique.js
+++ chal-make-unique-string/makeUnique.js
@@ -1,2 +1,12 @@
 // Write your solution below:
 console.log('hello world');
+
+function removeDuplicateCharacters(string) {
+    return string
+      .split('')
+      .filter(function(item, pos, self) {
+        return self.indexOf(item) == pos;
+      })
+      .join('');
+  }
+  console.log(removeDuplicateCharacters('baraban'));
