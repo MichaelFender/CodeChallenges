07380176b2c739128d39d503ebf24690fdeb3ef8
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-09 09:22:59 -0600

    changes var names

--- phoneNumber.js
+++ phoneNumber.js
@@ -1,12 +1,12 @@
-function createPhoneNumber(numbers){
-    var format = "(xxx) xxx-xxxx";
+function createPhoneNumber(phone){
+    var number = "(xxx) xxx-xxxx";
     
-    for(var i = 0; i < numbers.length; i++)
+    for(var i = 0; i < number.length; i++)
     {
-      format = format.replace('x', numbers[i]);
+      number = number.replace('x', phone[i]);
     }
     
-    return format;
+    return number;
   }
 
 console.log(createPhoneNumber('1234567890'));
