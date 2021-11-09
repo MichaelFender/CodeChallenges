0107d625dc4fd82ba3308999800946faf07d791a
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-09 09:19:50 -0600

    done

---
+++ phoneNumber.js
@@ -0,0 +1,12 @@
+function createPhoneNumber(numbers){
+    var format = "(xxx) xxx-xxxx";
+    
+    for(var i = 0; i < numbers.length; i++)
+    {
+      format = format.replace('x', numbers[i]);
+    }
+    
+    return format;
+  }
+
+console.log(createPhoneNumber('1234567890'));
