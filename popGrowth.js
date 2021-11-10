bf3c4e887e6fe82119cf56b528b2c2402d6ebee2
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-10 09:25:50 -0600

    pair coding with Jeeramel

---
+++ popGrowth.js
@@ -0,0 +1,10 @@
+function nbYear(p0, percent, aug, p) {
+    let yearCount = 0;
+    while (p0 <= p) {
+        p0 += p0 * (percent / 100) + aug;
+        yearCount++;
+    }
+    return yearCount;
+}
+console.log(nbYear(1500, 5, 100, 5000));
+console.log(nbYear(1500000, 2.5, 10000, 2000000));
