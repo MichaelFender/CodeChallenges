d9ccc9ac5d8fc366d152fa09fd1d76abe49d9c36
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-15 09:40:29 -0600

    done

---
+++ chal-build-tower/buildTower.js
@@ -0,0 +1,14 @@
+function towerBuilder(nFloors) {
+    const tower = []
+    let towerString = '*'
+      for(let i = 0; i<nFloors; i++){ 
+        tower.push(towerString)
+        towerString += "**"
+        for (let j = 0; j<i; j++){
+          tower[j] = " " + tower[j] + ' '
+        }
+      }
+      return tower
+    }
+
+    console.log(towerBuilder(10));
